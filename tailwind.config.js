32ed9bed681a8b8ce61486cf5e9195644882150c
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-07-02 23:51:43 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsxs,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
