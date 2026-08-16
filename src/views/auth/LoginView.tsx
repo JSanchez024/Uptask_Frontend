72690117e84a553d0836eae6ba9baf50b0dfd3b1
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-08-15 23:52:05 -0400

    avance V2.2

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -28,9 +28,17 @@ export default function LoginView() {
 
   return (
     <>
+      <h1 className="text-5xl font-black text-white">Iniciar Sesion</h1>
+      <p className="text-2xl font-light text-white mt-5">
+          Comienza a planear tus proyectos  {''}
+          <span className=" text-fuchsia-500 font-bold"> iniciando sesion en este formulario</span>
+      </p>
+
+
       <form
         onSubmit={handleSubmit(handleLogin)}
-        className="space-y-8 p-10 bg-white"
+        className="space-y-8 p-10 mt
+         bg-white"
         noValidate
       >
         <div className="flex flex-col gap-5">
@@ -86,6 +94,11 @@ export default function LoginView() {
             to={'/auth/register'}
             className="text-center text-gray-300 font-normal"
           >¿No tienes cuenta? Crear Una</Link>
+
+          <Link
+            to={'/auth/forgot-password'}
+            className="text-center text-gray-300 font-normal"
+          >¿Olvidaste tu contraseña? Reestablecer</Link>
       </nav>
     </>
   )
---
+++ src/views/auth/NewPasswordView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewPasswordView() {
+  return (
+    <div>NewPasswordView</div>
+  )
+}
