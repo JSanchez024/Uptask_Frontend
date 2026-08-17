7615d0b7ac5858aac56846d0a9a1586396d7fdb2
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-08-16 23:37:00 -0400

    avance V2.3

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function NewPasswordView() {
   return (
-    <div>NewPasswordView</div>
+    <>
+      <h1 className="text-5xl font-black text-white">Crear Cuenta</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Llena el formulario para {''}
+        <span className=" text-fuchsia-500 font-bold"> crear tu cuenta</span>
+      </p>
+    </>
   )
 }
